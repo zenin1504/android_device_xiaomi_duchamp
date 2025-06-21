290efbfc26dc284b35f818eeff64e3fd1edfe488
zenin1504 <kayozzprojects@yandex.com>
2025-06-21 23:22:30 +0000

    duchamp: Drop lineage compat
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,3 @@
-cd hardware/lineage/compat
-git fetch https://github.com/xiaomi-mt6897-duchamp/android_hardware_lineage_compat
-git cherry-pick 9a046ea7e5899adc38ab04fe24eb34859fe4d779
-cd ../../../
-
 cd external/wpa_supplicant_8
 git fetch https://github.com/xiaomi-mt6897-duchamp/android_external_wpa_supplicant_8
 git cherry-pick cc88629c6c5c2c2353bf87efef9b5c9c5bf32bee
